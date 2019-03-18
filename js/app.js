31aa955533447d8f3b22c3f029b96698bfc5e2dc
Alex Colombo <uricaine@hotmail.com>
2019-03-18 12:52:57 +0100

    init pouchDB

--- js/app.js
+++ js/app.js
@@ -1,148 +1,136 @@
 (function() {
+	'use strict';
 
-  'use strict';
+	var ENTER_KEY = 13;
+	var newTodoDom = document.getElementById('new-todo');
+	var syncDom = document.getElementById('sync-wrapper');
 
-  var ENTER_KEY = 13;
-  var newTodoDom = document.getElementById('new-todo');
-  var syncDom = document.getElementById('sync-wrapper');
+	// EDITING STARTS HERE (you dont need to edit anything above this line)
 
-  // EDITING STARTS HERE (you dont need to edit anything above this line)
+	var db = new PouchDB('todos');
+	var remoteCouch = false;
 
-  var db = new PouchDB('todos');;
-  var remoteCouch = false;
+	// We have to create a new todo document and enter it in the database
+	function addTodo(text) {
+		var todo = {
+			_id: new Date().toISOString(),
+			title: text,
+			completed: false
+		};
 
-  // We have to create a new todo document and enter it in the database
-  function addTodo(text) {
+		// db.put(todo, function callback(err, result) {
+		//   if (!err) {
+		//     console.log('Successfully posted a todo!');
+		//   }
+		// });
 
-    var todo = {
-      _id: new Date().toISOString(),
-      title: text,
-      completed: false
-    };
+		db.put(todo).then(console.log('todo insertado')).catch(console.log);
+	}
 
-    // db.put(todo, function callback(err, result) {
-    //   if (!err) {
-    //     console.log('Successfully posted a todo!');
-    //   }
-    // });
+	// Show the current list of todos by reading them from the database
+	function showTodos() {}
 
-    db.put(todo)
-      .then(console.log('todo insertado'))
-      .catch(console.log)
+	function checkboxChanged(todo, event) {}
 
+	// User pressed the delete button for a todo, delete it
+	function deleteButtonPressed(todo) {}
 
-  }
+	// The input box when editing a todo has blurred, we should save
+	// the new title or delete the todo if the title is empty
+	function todoBlurred(todo, event) {}
 
-  // Show the current list of todos by reading them from the database
-  function showTodos() {
-  }
+	// Initialise a sync with the remote server
+	function sync() {}
 
-  function checkboxChanged(todo, event) {
-  }
+	// EDITING STARTS HERE (you dont need to edit anything below this line)
 
-  // User pressed the delete button for a todo, delete it
-  function deleteButtonPressed(todo) {
-  }
+	// There was some form or error syncing
+	function syncError() {
+		syncDom.setAttribute('data-sync-state', 'error');
+	}
 
-  // The input box when editing a todo has blurred, we should save
-  // the new title or delete the todo if the title is empty
-  function todoBlurred(todo, event) {
-  }
+	// User has double clicked a todo, display an input so they can edit the title
+	function todoDblClicked(todo) {
+		var div = document.getElementById('li_' + todo._id);
+		var inputEditTodo = document.getElementById('input_' + todo._id);
+		div.className = 'editing';
+		inputEditTodo.focus();
+	}
 
-  // Initialise a sync with the remote server
-  function sync() {
-  }
+	// If they press enter while editing an entry, blur it to trigger save
+	// (or delete)
+	function todoKeyPressed(todo, event) {
+		if (event.keyCode === ENTER_KEY) {
+			var inputEditTodo = document.getElementById('input_' + todo._id);
+			inputEditTodo.blur();
+		}
+	}
 
-  // EDITING STARTS HERE (you dont need to edit anything below this line)
+	// Given an object representing a todo, this will create a list item
+	// to display it.
+	function createTodoListItem(todo) {
+		var checkbox = document.createElement('input');
+		checkbox.className = 'toggle';
+		checkbox.type = 'checkbox';
+		checkbox.addEventListener('change', checkboxChanged.bind(this, todo));
 
-  // There was some form or error syncing
-  function syncError() {
-    syncDom.setAttribute('data-sync-state', 'error');
-  }
+		var label = document.createElement('label');
+		label.appendChild(document.createTextNode(todo.title));
+		label.addEventListener('dblclick', todoDblClicked.bind(this, todo));
 
-  // User has double clicked a todo, display an input so they can edit the title
-  function todoDblClicked(todo) {
-    var div = document.getElementById('li_' + todo._id);
-    var inputEditTodo = document.getElementById('input_' + todo._id);
-    div.className = 'editing';
-    inputEditTodo.focus();
-  }
+		var deleteLink = document.createElement('button');
+		deleteLink.className = 'destroy';
+		deleteLink.addEventListener('click', deleteButtonPressed.bind(this, todo));
 
-  // If they press enter while editing an entry, blur it to trigger save
-  // (or delete)
-  function todoKeyPressed(todo, event) {
-    if (event.keyCode === ENTER_KEY) {
-      var inputEditTodo = document.getElementById('input_' + todo._id);
-      inputEditTodo.blur();
-    }
-  }
+		var divDisplay = document.createElement('div');
+		divDisplay.className = 'view';
+		divDisplay.appendChild(checkbox);
+		divDisplay.appendChild(label);
+		divDisplay.appendChild(deleteLink);
 
-  // Given an object representing a todo, this will create a list item
-  // to display it.
-  function createTodoListItem(todo) {
-    var checkbox = document.createElement('input');
-    checkbox.className = 'toggle';
-    checkbox.type = 'checkbox';
-    checkbox.addEventListener('change', checkboxChanged.bind(this, todo));
+		var inputEditTodo = document.createElement('input');
+		inputEditTodo.id = 'input_' + todo._id;
+		inputEditTodo.className = 'edit';
+		inputEditTodo.value = todo.title;
+		inputEditTodo.addEventListener('keypress', todoKeyPressed.bind(this, todo));
+		inputEditTodo.addEventListener('blur', todoBlurred.bind(this, todo));
 
-    var label = document.createElement('label');
-    label.appendChild( document.createTextNode(todo.title));
-    label.addEventListener('dblclick', todoDblClicked.bind(this, todo));
+		var li = document.createElement('li');
+		li.id = 'li_' + todo._id;
+		li.appendChild(divDisplay);
+		li.appendChild(inputEditTodo);
 
-    var deleteLink = document.createElement('button');
-    deleteLink.className = 'destroy';
-    deleteLink.addEventListener( 'click', deleteButtonPressed.bind(this, todo));
+		if (todo.completed) {
+			li.className += 'complete';
+			checkbox.checked = true;
+		}
 
-    var divDisplay = document.createElement('div');
-    divDisplay.className = 'view';
-    divDisplay.appendChild(checkbox);
-    divDisplay.appendChild(label);
-    divDisplay.appendChild(deleteLink);
+		return li;
+	}
 
-    var inputEditTodo = document.createElement('input');
-    inputEditTodo.id = 'input_' + todo._id;
-    inputEditTodo.className = 'edit';
-    inputEditTodo.value = todo.title;
-    inputEditTodo.addEventListener('keypress', todoKeyPressed.bind(this, todo));
-    inputEditTodo.addEventListener('blur', todoBlurred.bind(this, todo));
+	function redrawTodosUI(todos) {
+		var ul = document.getElementById('todo-list');
+		ul.innerHTML = '';
+		todos.forEach(function(todo) {
+			ul.appendChild(createTodoListItem(todo.doc));
+		});
+	}
 
-    var li = document.createElement('li');
-    li.id = 'li_' + todo._id;
-    li.appendChild(divDisplay);
-    li.appendChild(inputEditTodo);
+	function newTodoKeyPressHandler(event) {
+		if (event.keyCode === ENTER_KEY) {
+			addTodo(newTodoDom.value);
+			newTodoDom.value = '';
+		}
+	}
 
-    if (todo.completed) {
-      li.className += 'complete';
-      checkbox.checked = true;
-    }
+	function addEventListeners() {
+		newTodoDom.addEventListener('keypress', newTodoKeyPressHandler, false);
+	}
 
-    return li;
-  }
+	addEventListeners();
+	showTodos();
 
-  function redrawTodosUI(todos) {
-    var ul = document.getElementById('todo-list');
-    ul.innerHTML = '';
-    todos.forEach(function(todo) {
-      ul.appendChild(createTodoListItem(todo.doc));
-    });
-  }
-
-  function newTodoKeyPressHandler( event ) {
-    if (event.keyCode === ENTER_KEY) {
-      addTodo(newTodoDom.value);
-      newTodoDom.value = '';
-    }
-  }
-
-  function addEventListeners() {
-    newTodoDom.addEventListener('keypress', newTodoKeyPressHandler, false);
-  }
-
-  addEventListeners();
-  showTodos();
-
-  if (remoteCouch) {
-    sync();
-  }
-
+	if (remoteCouch) {
+		sync();
+	}
 })();
