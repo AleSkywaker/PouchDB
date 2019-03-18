13334a24294dc874e8e029c2b6fe59f44918312a
Alex Colombo <uricaine@hotmail.com>
2019-03-18 12:58:13 +0100

    init pouchDB

--- js/app.js
+++ js/app.js
@@ -29,9 +29,9 @@
 
 	// Show the current list of todos by reading them from the database
 	function showTodos() {
-		db.allDocs({ include_docs: true, descending: true }, function(err, doc) {
-			redrawTodosUI(doc.rows);
-		});
+		// db.allDocs({ include_docs: true, descending: true }, function(err, doc) {
+		// 	redrawTodosUI(doc.rows);
+		// });
 	}
 
 	function checkboxChanged(todo, event) {}
