ec9665f41d042a47605c94ce4a0d620141360472
Alex Colombo <uricaine@hotmail.com>
2019-03-23 09:02:50 +0100

    adding js files to app shell

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -1,13 +1,24 @@
 
 
-//Guardar cache dinamico
-function actualizaCacheDinamico(dynamicCache, req, res){
-     if(res.ok){
-         return caches.open(dynamicCache).then(cache=>{
-             cache.put(req, res.clone());
-             return res.clone();
-         })
-     }else{
-         return res;
-     }
+// Guardar  en el cache dinamico
+function actualizaCacheDinamico( dynamicCache, req, res ) {
+
+
+    if ( res.ok ) {
+
+        return caches.open( dynamicCache ).then( cache => {
+
+            cache.put( req, res.clone() );
+            
+            return res.clone();
+
+        });
+
+    } else {
+        return res;
+    }
+
+
+
 }
+
